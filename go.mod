a97068fa34b826e1126c335a67240b4042751d1a
Ashley Rhoten <ashleydawnrhoten@gmail.com>
2021-01-14 22:10:07 +0000

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/tensor-programming/golang-blockchain
+
+go 1.14
+
+require github.com/replit/database-go v0.0.0-20200814154522-f5159eae301b
---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+  "fmt"
+  "blockchain"
+)
+
+
+
+func main() {
+  chain := InitBlockChain()
+
+  chain.AddBlock("2nd Block")
+  chain.AddBlock("3rd Block")
+  chain.AddBlock("4th Block")
+
+  for _, block := range chain.blocks {
+    fmt.Printf("Previous Hash: %x\n", block.PrevHash)
+    fmt.Printf("Data in Block: %s\n", block.Data)
+    fmt.Printf("Hash: %x\n", block.Hash)
+    fmt.Println()
+
+  }
+}
